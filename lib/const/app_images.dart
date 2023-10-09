2d42c8584d9875c568d622d5eb6fa62959acfc97
syaiful919 <syaiful919@gmail.com>
2023-10-09 09:46:44 +0700

    feat: add assets

---
+++ lib/const/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  const AppImages._();
+
+  static const String _basePath = 'assets/images/';
+  static const String poisonHeader = '${_basePath}img_poison_header.png';
+  static const String zubat = '${_basePath}img_zubat.gif';
+}

9e93fec1e1930ded6de1d14099da926818c08222
syaiful919 <syaiful919@gmail.com>
2023-10-13 23:43:58 +0700

    refactor: move text shimmer to it's own file

--- lib/ui/pages/detail/widgets/detail_page_shimmer.dart
+++ lib/ui/pages/detail/widgets/detail_page_shimmer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
+import 'text_shimmer.dart';
 
 class DetailPageShimmer extends StatelessWidget {
   const DetailPageShimmer({super.key});
@@ -50,33 +51,6 @@ class DetailPageShimmer extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class TextShimmer extends StatelessWidget {
-  final double width;
-  final double height;
-
-  const TextShimmer({
-    super.key,
-    this.width = 100,
-    this.height = 20,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Shimmer.fromColors(
-      baseColor: Colors.black.withOpacity(0.2),
-      highlightColor: Colors.black.withOpacity(0.3),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: Colors.white,
-        ),
-        height: height,
-        width: width,
       ),
     );
   }

ce3ceebd425bd14973b52c63b5680843afca2d61
syaiful919 <syaiful919@gmail.com>
2023-10-13 22:23:02 +0700

    feat: display patch version

--- lib/services/app_version_service.dart
+++ lib/services/app_version_service.dart
@@ -1,12 +1,25 @@
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:shorebird_code_push/shorebird_code_push.dart';
 
 class AppVersionService {
-  const AppVersionService();
+  final ShorebirdCodePush codePush;
+
+  const AppVersionService({
+    required this.codePush,
+  });
 
   Future<String> getAppVersion() async {
     try {
       final PackageInfo packageInfo = await PackageInfo.fromPlatform();
-      return packageInfo.version.trim();
+
+      String appVersion = packageInfo.version.trim();
+      final int? currentPatchversion = await codePush.currentPatchNumber();
+
+      if (currentPatchversion != null) {
+        appVersion = '$appVersion - $currentPatchversion';
+      }
+
+      return appVersion;
     } catch (_) {
       return '';
     }
